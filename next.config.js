cd8fb6f5c92b712b8431db6e57b898411161aef5
jimmortel <jimmp2010@gmail.com>
2026-05-22 23:59:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Force l'utilisation du moteur de build standard sans Turbopack pour la stabilité
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
