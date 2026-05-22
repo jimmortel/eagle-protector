d901a79102af9d141655c1d29c49e78f79bb85fb
jimmortel <jimmp2010@gmail.com>
2026-05-22 23:31:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Désactive l'optimisation des images pour éviter les erreurs de build
-  images: {
-    unoptimized: true,
-  },
-}
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
